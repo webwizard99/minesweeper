20eef22b948ecb328ddd9c4df783c97d1137fffa
Bezlonir <michael_bragg99@hotmail.com>
2018-09-18 12:07:21 -0600

    Create initial file structure.

---
+++ resources/js/main.js
@@ -0,0 +1,21 @@
+const MinefieldController = function(){
+
+}();
+
+const UIController = function(){
+
+}();
+
+const Controller = function(){
+    const setEventListeners = function() {
+
+    }
+    
+    return {
+        init: function() {
+
+        }
+    }
+}();
+
+Controller.init();
